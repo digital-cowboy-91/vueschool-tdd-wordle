0529c83868253b76a0f431c0d2f076602cc946b2
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-02 17:24:49 +0000

    use valid word of the day

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import WordleBoard from './components/WordleBoard.vue'
+import WordleBoard from "./components/WordleBoard.vue";
 </script>
 
 <template>
-  <WordleBoard wordOfTheDay="You did it!" />
+  <WordleBoard wordOfTheDay="TESTS" />
 </template>
