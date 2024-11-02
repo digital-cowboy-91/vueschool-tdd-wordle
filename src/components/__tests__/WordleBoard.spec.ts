3f318f1e3e17b2d37c44678cfe310bf9040294f3
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-02 15:10:13 +0000

    adds typescript

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,14 +1,16 @@
-import { mount } from "@vue/test-utils";
+import { DOMWrapper, mount, VueWrapper } from "@vue/test-utils";
 import WordleBoard from "../WordleBoard.vue";
 import { VICTORY_MESSAGE } from "@/settings";
 
 describe("WordleBoard", () => {
-  test("a victory message appears when the user makes a guess that matches the word of the day", async () => {
+  test("a victory message appears when the user makes a guess that matches the word of the day", async (): Promise<void> => {
     // Arrange
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: "TESTS" } });
+    const wrapper: VueWrapper = mount(WordleBoard, {
+      props: { wordOfTheDay: "TESTS" },
+    });
 
     // Act
-    const guessInput = wrapper.find("input[type=text]");
+    const guessInput: DOMWrapper<Element> = wrapper.find("input[type=text]");
     await guessInput.setValue("TESTS");
     await guessInput.trigger("keydown.enter");
 
