831b7c54850c581617dd2e06be8c376acfdd59a3
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-02 15:44:39 +0000

    enforce the wordOfTheDay must have 5 characters

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -34,4 +34,12 @@ describe("WordleBoard", () => {
     expect(wrapper.text()).not.toContain(VICTORY_MESSAGE);
     expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE);
   });
+
+  test("if a word of the day provided does not have exactly 5 chars, emits warning", async (): Promise<void> => {
+    vi.spyOn(console, "warn");
+
+    mount(WordleBoard, { props: { wordOfTheDay: "SPY" } });
+
+    expect(console.warn).toHaveBeenCalled();
+  });
 });
