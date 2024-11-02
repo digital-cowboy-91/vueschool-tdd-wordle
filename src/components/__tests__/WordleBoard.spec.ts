fa84afaabb9895d0cd90342a672994e305bf7d9e
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-02 15:08:51 +0000

    extract constant

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,17 +1,18 @@
-import { mount } from '@vue/test-utils'
-import WordleBoard from '../WordleBoard.vue'
+import { mount } from "@vue/test-utils";
+import WordleBoard from "../WordleBoard.vue";
+import { VICTORY_MESSAGE } from "@/settings";
 
-describe('WordleBoard', () => {
-  test('a victory message appears when the user makes a guess that matches the word of the day', async() => {
+describe("WordleBoard", () => {
+  test("a victory message appears when the user makes a guess that matches the word of the day", async () => {
     // Arrange
-    const wrapper = mount(WordleBoard, {props: {wordOfTheDay: 'TESTS'}})
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: "TESTS" } });
 
     // Act
-    const guessInput = wrapper.find("input[type=text]")
-    await guessInput.setValue('TESTS')
-    await guessInput.trigger('keydown.enter')
+    const guessInput = wrapper.find("input[type=text]");
+    await guessInput.setValue("TESTS");
+    await guessInput.trigger("keydown.enter");
 
     // Assert
-    expect(wrapper.text()).toContain("You won!")
-  })
-})
+    expect(wrapper.text()).toContain(VICTORY_MESSAGE);
+  });
+});
